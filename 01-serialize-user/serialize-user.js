910c479622cf1d1e4ee88aa94a4b4a1ae3a3d75f
amalakamala <amalakamala.a@gmail.com>
2017-10-01 12:50:08 -0300

    subir vacio

--- 01-serialize-user/serialize-user.js
+++
@@ -1,8 +0,0 @@
-const serializeUser = user => {
-    const userA =  Object.assign({}, user)   
-	userA.name = (userA.name.length > 10) ? `${userA.name.slice(0, 7)}...` : userA.name;
-	userA.date = userA.date.toJSON();
-	return JSON.stringify(userA);
-};
-
-module.exports = serializeUser;
--- 03-parse-items/parse-items.js
+++
@@ -1,6 +0,0 @@
-const parseItems = (items) => {
-	const itemsA =  Object.assign([], items)  
-   	return itemsA.map(e=>parseInt(e)).sort()
-};
-//Number.isInteger(123) = true
-module.exports = parseItems;
